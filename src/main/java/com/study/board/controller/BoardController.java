b2732fb7df3f7c692b98ccbebc271a3959d19f0c
c1oud-dev <gksmf0187@gmail.com>
2023-10-24 15:28:24 +0900

    add list page, view page

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,5 +25,18 @@ public class BoardController {
         boardService.write(board);
 
         return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+        model.addAttribute("List", boardService.boardList());
+
+        return "boardList";
+    }
+
+    @GetMapping("/board/view") //localhost:8080/board/view?id=1 (get 방식이라고 한다.)
+    public String boardView(Model model, Integer id) {
+        model.addAttribute("article", boardService.boardView(id));
+        return "boardview";
     }
 }
